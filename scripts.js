dc93436d73e208ca86201f1686cccf69ee156d58
Lurppi <dennis.uhlig@icloud.com>
2024-08-17 14:50:04 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // Accordion functionality
     var acc = document.getElementsByClassName("accordion");
     for (var i = 0; i < acc.length; i++) {
         acc[i].addEventListener("click", function() {
@@ -11,4 +12,28 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
     }
+
+    // Countdown functionality
+    function countdown() {
+        var countDownDate = new Date("October 13, 2024 13:00:00").getTime();
+        var x = setInterval(function() {
+            var now = new Date().getTime();
+            var distance = countDownDate - now;
+
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+            document.getElementById("countdown").innerHTML = "Season starts in: " + days + "d " + hours + "h "
+            + minutes + "m " + seconds + "s ";
+
+            if (distance < 0) {
+                clearInterval(x);
+                document.getElementById("countdown").innerHTML = "The season has started!";
+            }
+        }, 1000);
+    }
+
+    countdown();
 });
