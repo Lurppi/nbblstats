078d7ec97cffd052ee850fd2aa136dfe2c66f3d5
Lurppi <dennis.uhlig@icloud.com>
2024-08-17 14:23:35 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,12 +1,14 @@
-// JavaScript for dropdown functionality
-document.addEventListener('DOMContentLoaded', function() {
-    const dropdowns = document.querySelectorAll('nav ul li');
-    dropdowns.forEach(dropdown => {
-        dropdown.addEventListener('mouseover', function() {
-            this.querySelector('.dropdown-content').style.display = 'block';
+document.addEventListener("DOMContentLoaded", function() {
+    var acc = document.getElementsByClassName("accordion");
+    for (var i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
         });
-        dropdown.addEventListener('mouseout', function() {
-            this.querySelector('.dropdown-content').style.display = 'none';
-        });
-    });
+    }
 });
