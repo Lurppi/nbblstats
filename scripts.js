9f5ce1a43fcc9d895fae29d034008e9d1a4f7d21
Lurppi <dennis.uhlig@icloud.com>
2024-08-17 15:22:55 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -22,4 +22,11 @@ document.addEventListener('DOMContentLoaded', function() {
         const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-        document.getElementById("countdown").innerHTML = `${days}d ${hours
+        document.getElementById("countdown").innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+
+        if (distance < 0) {
+            clearInterval(countdownFunction);
+            document.getElementById("countdown").innerHTML = "EXPIRED";
+        }
+    }, 1000);
+});
