e3c8970cbb3f0357cacf9a991bd2bb6446e7cf94
Lurppi <dennis.uhlig@icloud.com>
2024-08-17 13:55:40 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,12 +1,21 @@
-function showSection(sectionId) {
-    document.querySelectorAll('.section').forEach(function(section) {
-        section.style.display = 'none';
+document.addEventListener('DOMContentLoaded', function() {
+    // Navigation Handling
+    document.querySelectorAll('nav ul li a').forEach(function(link) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            let targetId = this.getAttribute('href').replace('.html', '');
+            showSection(targetId);
+        });
     });
-    document.getElementById(sectionId).style.display = 'block';
-}
 
-// Initialisieren der DataTables
-$(document).ready(function() {
+    function showSection(sectionId) {
+        document.querySelectorAll('section').forEach(function(section) {
+            section.style.display = 'none';
+        });
+        document.getElementById(sectionId).style.display = 'block';
+    }
+
+    // DataTables Initialization
     $('#teams-table').DataTable();
     $('#players-table').DataTable();
     $('#roster-table').DataTable();
